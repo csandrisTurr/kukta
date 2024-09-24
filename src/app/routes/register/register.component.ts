17ee76e56ca3f16dfff3f21be9c87ef038e4c4a8
csandrisTurr <csaszarandras@turr.hu>
2024-09-24 10:59:50 +0200

    feat: szebb auth

--- src/app/routes/register/register.component.ts
+++ src/app/routes/register/register.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { ButtonComponent } from '../../components/button/button.component';
+import { InputComponent } from '../../components/input/input.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [InputComponent, ButtonComponent, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  name = new FormControl('');
+  email = new FormControl('');
+  password = new FormControl('');
+  phone = new FormControl('');
 
+  register() {
+
+  }
 }
