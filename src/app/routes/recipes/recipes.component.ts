8f1dd03752086d8d8576e23ca3c8bf675bd630c5
csandrisTurr <csaszarandras@turr.hu>
2024-09-25 12:08:55 +0200

    feat: recipes oldal kinezet

--- src/app/routes/recipes/recipes.component.ts
+++ src/app/routes/recipes/recipes.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { IconComponent } from '../../components/icon/icon.component';
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [],
+  imports: [IconComponent],
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.scss'
 })
