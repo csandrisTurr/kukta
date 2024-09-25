9edf484ed2c68e4d34fabbde7440824f76da5b03
csandrisTurr <csaszarandras@turr.hu>
2024-09-25 12:30:55 +0200

    feat: en amikor kicsengetnek

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './routes/home/home.component';
 import { LoginComponent } from './routes/login/login.component';
 import { RegisterComponent } from './routes/register/register.component';
 import { RecipesComponent } from './routes/recipes/recipes.component';
+import { RecipeComponent } from './routes/recipe/recipe.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'recipes', component: RecipesComponent },
+    { path: 'recipe/:id', component: RecipeComponent },
 ];
