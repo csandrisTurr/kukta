463e93808faa0105e24a5929e626d2e290a17cd3
csandrisTurr <csaszarandras@turr.hu>
2024-09-30 09:41:33 +0200

    fix: import input to recipe page

--- src/app/routes/recipe/recipe.component.ts
+++ src/app/routes/recipe/recipe.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IconComponent } from '../../components/icon/icon.component';
 
 @Component({
   selector: 'app-recipe',
   standalone: true,
-  imports: [],
+  imports: [IconComponent],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.scss'
 })
